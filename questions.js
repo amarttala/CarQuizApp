55e305d22f1a459aed5059be3fa498bcbde22516
Andrew Marttala <andrewmarttala@gmail.com>
2020-04-09 12:14:18 -0700

    initial commit

---
+++ questions.js
@@ -0,0 +1,112 @@
+const QUESTIONS = [
+	{
+		question: "In 1998, what road car went 240.1 mph?",
+		choices: [
+			'Bugatti EB110',
+			'Lamborghini Diablo VT',
+			'Jaguar XJ220',
+			'McLaren F1'
+		],
+		correctAnswer: 3,
+		explanation: 'On March 31, 1998 Andy Wallace drove the McLaren F1 to a production car record of 240.1 mph (386.4 km/h).'
+	},
+	{
+		question: "Who is the most successful Formula 1 driver of all time?",
+		choices: [
+			'Lewis Hamilton',
+			'Michael Schumacher',
+			'Sebastian Vettel',
+			'Fernando Alonso'
+		],
+		correctAnswer: 1,
+		explanation: 'Statistically speaking, Michael Schumacher is the most successful F1 Driver with: 7 world championships, the most car maker&apos;s titles, race victories, pole positions and championship points scored.'
+	},
+	{
+		question: "In 1987, what Ferrari was built to take back the &quot;Fastest Car&quot; title from the Porsche 959?",
+		choices: [
+			'Ferrari F40',
+			'Ferrari 355',
+			'Ferrari F50',
+			'Ferrari 348'
+		],
+		correctAnswer: 0,
+		explanation: 'The Ferrari F40 went 201 mph (323.4 km/h), edging out the Porsche 959 with a top speed of 196 mph (317 km/h)'
+			},
+	{
+		question: "How many cylinders does the Lexus LFA have?",
+		choices: [
+			'6',
+			'8',
+			'10',
+			'12'
+		],
+		correctAnswer: 2,
+		explanation: 'The Lexus LFA has a handbuilt 4.8L (293.2 cu/in) V10 producing 553 hp at 8,700 rpm.'
+	},
+	{
+		question: "What Formula 1 Team did both Niki Lauda and Juan Manuel Fangio drive for?",
+		choices: [
+			'McLaren',
+			'Williams',
+			'Ferrari',
+			'Lotus'
+		],
+		correctAnswer: 2,
+		explanation: 'Both drivers were with Ferrari, Niki Lauda from 1974 - 1977 and Juan Manuel Fangio in 1956.'
+	},
+	{
+		question: "In what country is Circuit de Spa-Francorchamps located?",
+		choices: [
+			'Belgium',
+			'France',
+			'Italy',
+			'Hungary'
+		],
+		correctAnswer: 0,
+		explanation: 'Circuit de Spa-Francorchamps is located in Stavelot, Belgium.  It is a 4.3 mile long circuit that hosts the Belgium Grand Prix.'
+	},
+	{
+		question: "What did Ferruccio Lamborghini produce when he founded his company?",
+		choices: [
+			'Sports Cars',
+			'Planes',
+			'Toys',
+			'Tractors'
+		],
+		correctAnswer: 3,
+		explanation: 'After World War II, Ferruccio Lamborghini founded Lamborghini. At the time, Lamborghini assembled tractors from reconfigured surplus military machines near Bologna, Italy.  It was not until 1963 that Ferruccio Lambhorgini produced luxury sports cars with Automobili Lamborghini.'
+	},
+	{
+		question: "What car company produces the Jesko and Regera?",
+		choices: [
+			'Pagani',
+			'Koenigsegg',
+			'Gumpert',
+			'Gemballa'
+		],
+		correctAnswer: 1,
+		explanation: 'Koenigsegg produced both the Jesko and Regera. It was founded by Christian von Koenigsegg in 1994 in &Auml;ngelholm, Sweeden.  Koenigsegg produces some of the fastest cars in the world.'
+	},
+	{
+		question: "&quot;The Ultimate Driving Machine&quot; is the brand slogan of which company?",
+		choices: [
+			'Mercedes',
+			'Audi',
+			'BMW',
+			'Porsche'
+		],
+		correctAnswer: 2,
+		explanation: 'The tagline has been used continuously by BMW since 1975.'
+	},
+	{
+		question: "Which race is held at the Circuit de la Sarthe?",
+		choices: [
+			'24 Hours of Le Mans',
+			'French Grand Prix',
+			'Bathurst 1000',
+			'Paris-Dakar Rally'
+		],
+		correctAnswer: 0,
+		explanation: 'Circuit de la Sarthe is home to the 24 Hours of Le Mans World Endurance Championship race.  The participating race cars will travel more than 3000 miles in the famous 24 hour race.'
+	}
+]
